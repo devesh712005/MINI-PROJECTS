3f4a4bf6298b7e952b1724995f4e542e9dbf1bbc
devesh <2k23.it1a.2310619@gmail.com>
2025-11-01 12:27:43 +0530

    first commit

---
+++ Amazon/index.js
@@ -0,0 +1,7 @@
+function linkopen(){
+    let element = document.querySelectorAll(".nav-second");
+    
+}
+
+let element = document.querySelectorAll(".nav-second");
+element.addEventListener('click',linkopen);
