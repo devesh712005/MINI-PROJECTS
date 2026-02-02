e9f3501c31a31ecee05debd2bd11ca80118ec6f1
devesh712005 <deveshsingh712005@gmail.com>
2026-02-02 21:31:25 +0530

    Add: Rabbitmq and notification-service

--- Task-App/task-service/index.js
+++ Task-App/task-service/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const amqp = require("amqplib");
 const mongoose = require("mongoose");
 const PORT = 3002;
 const app = express();
@@ -24,6 +24,25 @@ const TaskSchema = mongoose.Schema({
 
 const Task = mongoose.model("Task", TaskSchema);
 
+let channel, connection;
+
+async function connectRabbitMQWithRetry(retries = 5, delay = 3000) {
+  while (retries) {
+    try {
+      connection = await amqp.connect("amqp://rabbitmq_node");
+      channel = await connection.createChannel();
+      await channel.assertQueue("task_created");
+      console.log("Connected to rabbitMQ");
+      return;
+    } catch (error) {
+      console.error("RabbitMQ Connection Error: ", error.message);
+      retries--;
+      console.error("retrying again : ", retries);
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+}
+
 app.get("/tasks", async (req, res) => {
   const task = await Task.find();
   res.status(200).json(task);
@@ -34,6 +53,15 @@ app.post("/tasks", async (req, res) => {
   try {
     const task = new Task({ title, description, userId });
     await task.save();
+
+    const message = { taskId: task._id, userId, title };
+
+    if (!channel) {
+      return res.status(503).json({ error: "RabbitMQ not connected" });
+    }
+
+    channel.sendToQueue("task_created", Buffer.from(JSON.stringify(message)));
+
     res.status(201).json(task);
   } catch (error) {
     console.error("Error saving: ", error);
@@ -43,4 +71,5 @@ app.post("/tasks", async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Task service  is running at ${PORT}`);
+  connectRabbitMQWithRetry();
 });
